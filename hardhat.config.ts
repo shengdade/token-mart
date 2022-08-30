0f180994eb11e26e13958cba527ea1a3fd6be6ea
Dade Sheng <hackerdade@gmail.com>
2022-08-30 16:45:14 +0800

    deploy to goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox'
+import { config as dotenvConfig } from 'dotenv'
+import { HardhatUserConfig } from 'hardhat/config'
+
+dotenvConfig()
+
+const API_URL = process.env.API_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+}
 
-export default config;
+export default config
