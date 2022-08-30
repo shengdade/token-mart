8813078860ba3e43625a8af1b685b4ea571350bd
Dade Sheng <hackerdade@gmail.com>
2022-08-30 17:06:53 +0800

    etherscan verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,6 +6,7 @@ dotenvConfig()
 
 const API_URL = process.env.API_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
@@ -14,6 +15,9 @@ const config: HardhatUserConfig = {
       url: API_URL,
       accounts: [PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 }
 
