ccbb38089dbddcefac69f9ebfd3c7e6e7c1e0e54
Dade Sheng <hackerdade@gmail.com>
2022-09-12 08:59:41 +0800

    add framer-motion; scale larger when hovering card

--- frontend/components/collectable/CollectableCard.tsx
+++ frontend/components/collectable/CollectableCard.tsx
@@ -1,4 +1,5 @@
 import { AspectRatio, Card, createStyles, Group, Text } from '@mantine/core'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
 import Label from '../layout/Label'
@@ -24,33 +25,35 @@ export default function CollectableCard({
   const { classes } = useStyles()
 
   return (
-    <Link href={`/collectable/${id}`} passHref>
-      <Card shadow="sm" radius="md" withBorder component="a">
-        <Card.Section>
-          <AspectRatio ratio={2800 / 2100}>
-            <Image
-              src={image.replace('ipfs://', 'https://ipfs.io/ipfs/')}
-              layout="fill"
-              alt="Image"
-            />
-          </AspectRatio>
-        </Card.Section>
+    <motion.div whileHover={{ scale: 1.05 }}>
+      <Link href={`/collectable/${id}`} passHref>
+        <Card shadow="sm" radius="md" withBorder component="a">
+          <Card.Section>
+            <AspectRatio ratio={2800 / 2100}>
+              <Image
+                src={image.replace('ipfs://', 'https://ipfs.io/ipfs/')}
+                layout="fill"
+                alt="Image"
+              />
+            </AspectRatio>
+          </Card.Section>
 
-        <Text size="sm" weight={500} my="xs">
-          {name}
-        </Text>
+          <Text size="sm" weight={500} my="xs">
+            {name}
+          </Text>
 
-        <Group position="apart">
-          <Label label="Price">
-            <Price value={price} weight={500} size="sm" />
-          </Label>
-          <Label label="Available">
-            <Text weight={500} size="sm">
-              {stock}
-            </Text>
-          </Label>
-        </Group>
-      </Card>
-    </Link>
+          <Group position="apart">
+            <Label label="Price">
+              <Price value={price} weight={500} size="sm" />
+            </Label>
+            <Label label="Available">
+              <Text weight={500} size="sm">
+                {stock}
+              </Text>
+            </Label>
+          </Group>
+        </Card>
+      </Link>
+    </motion.div>
   )
 }
